f523953963fc50a91dcfdffba1e186ec551fe238
Toby Qin <toby.qin@ef.com>
2016-11-08 11:34:01 +0800

    add samples.

--- underscore_samples_comments.py
+++ underscore_samples_comments.py
@@ -4,41 +4,45 @@
 # In module level
 
 a = 'without underscore'
-a_ = 'single underscore suffix'  # not recommend
-a__ = 'double underscore suffix'  # not recommend
-
 _a = 'single underscore prefix'  # import ignore, private member
-_a_ = 'single underscore surround'  # not recommend
-_a__ = 'single prefix + double suffix'  # not recommend
-
-__a = 'double underscore prefix'  # not recommend
-__a__ = 'double underscore surround'  # not recommend
-__a_ = 'double prefix + single suffix'  # not recommend
-
-___a = 'multiple underscore prefix'  # not recommend
-___a___ = 'multiple underscore surround'  # not recommend
-
 _ = "just single score"  # one time used, import ignore
-__ = 'just double underscore'  # not recommend
 
 
 def f():
     return 'function: without underscore'
 
 
-def _f():    # private member, import ignore
+def _f():  # private member, import ignore
     return 'function: single underscore prefix'
 
 
-def _f_():  # not recommend
+# not recommend
+
+a_ = 'single underscore suffix'
+a__ = 'double underscore suffix'
+
+_a_ = 'single underscore surround'
+_a__ = 'single prefix + double suffix'
+
+__a = 'double underscore prefix'
+__a__ = 'double underscore surround'
+__a_ = 'double prefix + single suffix'
+
+___a = 'multiple underscore prefix'
+___a___ = 'multiple underscore surround'
+
+__ = 'just double underscore'
+
+
+def _f_():
     return 'function: single underscore surround'
 
 
-def __f():  # not recommend
+def __f():
     return 'function: double underscore prefix'
 
 
-def __f__():  # not recommend
+def __f__():
     return 'function: double underscore surround'
 
 
@@ -47,32 +51,15 @@ def __f__():  # not recommend
 class MyClass(object):
     # as class attribute
     a = 'without underscore'
-    a_ = 'single underscore suffix'  # not recommend
-    a__ = 'double underscore suffix'  # not recommend
-
     _a = 'single underscore prefix'
-    _a_ = 'single underscore surround'  # not recommend
-    _a__ = 'single prefix + double suffix'  # not recommend
-
-    __a = 'double underscore prefix'
+    __a = 'double underscore prefix'  # super private
     __a__ = 'double underscore surround'
-    __a_ = 'double prefix + single suffix'  # not recommend
 
-    ___a = 'multiple underscore prefix'  # not recommend
-    ___a___ = 'multiple underscore surround'  # not recommend
-
-    _ = "just single score"
-    __ = 'just double underscore'  # not recommend
-
-    # as class instance method
     def f(self):
         return 'instance method: without underscore ({})'.format(self)
 
     def _f(self):
         return 'instance method: single underscore prefix ({})'.format(self)
-
-    def _f_(self):  # not recommend
-        return 'instance method: single underscore surround ({})'.format(self)
 
     def __f(self):
         return 'instance method: double underscore prefix ({})'.format(self)
@@ -80,10 +67,28 @@ class MyClass(object):
     def __f__(self):
         return 'instance method: double underscore surround ({})'.format(self)
 
-    # as class static method
     @staticmethod
     def s():
         return "static method: without underscore"
+
+    # not recommend
+    a_ = 'single underscore suffix'
+    a__ = 'double underscore suffix'
+    _a_ = 'single underscore surround'
+    _a__ = 'single prefix + double suffix'
+    __a_ = 'double prefix + single suffix'
+    ___a = 'multiple underscore prefix'
+    ___a___ = 'multiple underscore surround'
+
+    _ = "just single score"
+    __ = 'just double underscore'
+
+    # as class instance method
+
+    def _f_(self):  # not recommend
+        return 'instance method: single underscore surround ({})'.format(self)
+
+    # as class static method
 
     @staticmethod
     def _s():
