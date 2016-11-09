02f17dfdc1a9c286c655d0ee80c365e791df234b
Toby Qin <toby.qin@ef.com>
2016-11-09 11:42:42 +0800

    update demo.

--- magic_methods.py
+++ magic_methods.py
@@ -4,8 +4,20 @@ class MagicClass(object):
     #     return 'give me five!'
 
     def __init__(self, *args, **kwargs):
+        self.title = 'qa'
         print 'do something in __init__({},{})'.format(args, kwargs)
 
+    def __cmp__(self, other):
+        return 1
+
+    def __getattr__(self, item):
+        print 'get attribute for : {}'.format(item)
+        return 'hello'
+
+    def __getattribute__(self, item):
+        print 'get attribute for existed item: {}'.format(item)
+        return object.__getattribute__(self, item)
 
 mc = MagicClass(1, 2, 3)
-print mc
+print mc.title
+
